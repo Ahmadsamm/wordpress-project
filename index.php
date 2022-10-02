d5f49dbe25dc99a7e1c2acbbb5af829548ced0f0
unknown <ahmadalsamman.de@gmail.com>
2022-10-02 09:30:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Webseite</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+  <main>
+    <div class="container py-5">
+      <h1>Webseite</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eius neque obcaecati vero debitis, ut, autem minus velit ratione temporibus natus? Aliquam minus nesciunt, molestias labore aspernatur tempora eum fugit.</p>
+
+
+
+      <h1>
+        <?= 'hello world'; ?>
+      </h1>
+    </div>
+  </main>
+  <script>
+    'use strict';
+  </script>
+</body>
+
+</html>
